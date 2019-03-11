1d4e87399180b8567258b36a39e5db93ff78e9e2
mamer27 <mamerestevesgom@gmail.com>
2019-03-11 10:50:09 -0300

    passaro voando

--- Assets/Script/simplemove.cs
+++ Assets/Script/simplemove.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class simplemove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float SpeedFactor =  1f;
+    
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        float So = transform.position.x;
+        float S = So + SpeedFactor * Time.deltaTime;
+        transform.position = new Vector3(S, transform.position.y, transform.position.x);
     }
 }
