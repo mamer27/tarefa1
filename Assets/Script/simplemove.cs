ef25d87596e24fab5b6058a430eb8a9b31115a76
mamer27 <mamerestevesgom@gmail.com>
2019-03-18 09:45:33 -0300

    limites de tela

--- Assets/Script/simplemove.cs
+++ Assets/Script/simplemove.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class simplemove : MonoBehaviour
-{
-    private float SpeedFactor =  1f;
-    
+{   [SerializeField]
+    private float SpeedFactor =  100f;
+    [SerializeField]
+    private float LimitTop = 4f;
+    [SerializeField]
+    private float LimitBottom = -4f;
+    [SerializeField]
+    private float LimitLeft =  -8f;
+    [SerializeField]
+    private float LimitRight = 8f;
+
     // Start is called before the first frame update
     
     
@@ -23,5 +31,28 @@ public class simplemove : MonoBehaviour
         Vector3  velocity = new Vector3(horizontalInput, verticallInput, 0f) * SpeedFactor;
         
         transform.position = transform.position + velocity * Time.deltaTime;
+
+        if (transform.position.y < LimitBottom) {
+
+            transform.position = new Vector3(transform.position.x, LimitBottom, transform.position.z);
+        }
+    
+        if (transform.position.y > LimitTop) {
+
+
+            transform.position = new Vector3(transform.position.x, LimitTop, transform.position.z);
+
+        }
+    
+         if (transform.position.x < LimitLeft) {
+
+            transform.position = new Vector3(LimitLeft, transform.position.y, transform.position.z);
+        }
+
+         if (transform.position.x > LimitRight) {
+
+            transform.position = new Vector3(LimitRight, transform.position.y, transform.position.z);
+        }
+    
     }
 }
