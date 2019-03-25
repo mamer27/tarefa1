365d8122ef85fd39a81b0257ad032efeac5fb71d
mamer27 <mamerestevesgom@gmail.com>
2019-03-25 11:02:09 -0300

    rigidbody no movimento

---
+++ Assets/Script/movebyrigidbody.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movebyrigidbody : MonoBehaviour
+{   
+    [SerializeField]
+    
+    private float SpeedFactor = 10f;
+    
+    [SerializeField]
+    private Rigidbody2D Rb;
+    
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticallInput = Input.GetAxisRaw("Vertical");
+        
+        Rb.Velocity = new Vector3(horizontalInput, verticallInput, 0f)* SpeedFactor;
+    }
+}
