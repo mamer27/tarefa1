ffd318ac001fdac2a806b4108ea36391e2a7fd5d
mamer27 <mamerestevesgom@gmail.com>
2019-04-08 00:07:03 -0300

    layers personalizados e limites para movimentos dos players

--- Assets/Script/movebyrigidbody.cs
+++ Assets/Script/movebyrigidbody.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movebyrigidbody : MonoBehaviour
-{   
+{
     [SerializeField]
-    
+
     private float SpeedFactor = 10f;
-    
+
     [SerializeField]
     private Rigidbody2D Rb;
-    
+
     void Start()
     {
         Rb = GetComponent<Rigidbody2D>();
 
     }
 
-    
+
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticallInput = Input.GetAxisRaw("Vertical");
-        
-        Rb.Velocity = new Vector3(horizontalInput, verticallInput, 0f)* SpeedFactor;
+
+        Rb.velocity = new Vector3(horizontalInput, verticallInput, 0f) * SpeedFactor;
     }
 }
