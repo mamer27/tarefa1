34b75b4ff03c09beb31b04c5dc4053df59a35bad
mamer27 <mamerestevesgom@gmail.com>
2019-04-08 11:01:11 -0300

    soltandoraiozin

---
+++ Assets/Script/raiozin.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raiozin : MonoBehaviour
+{
+
+    public GameObject parent;
+    public GameObject prefab;
+    public GameObject spawnpoint;
+    public float waitTime = 3f;
+    public float raiospeed = 3f;
+    public Vector3[] directions;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GameObject.Find("Tape");
+        StartCoroutine(Spawn());
+    }
+
+            IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            for (int i = 0; i<directions.Length; i++)
+            {
+                GameObject projectile = Instantiate(prefab, spawnpoint.transform.position, Quaternion.identity);
+                projectile.transform.SetParent(parent.transform);
+                projectile.GetComponent<Rigidbody2D>().velocity = raiospeed * directions[i];
+
+
+
+            }
+
+
+        }
+
+
+    }
+}
